3b901f6aa536d92dddaf2a959152cd215329696c
Chen-Kuan Liao <czl5899@psu.edu>
2022-07-04 02:29:13 -0400

    adding commands

--- scr/script.js
+++ scr/script.js
@@ -5,7 +5,15 @@ var terminal_outputs = document.getElementById("terminal_outputs");
 function execute(input){
     let output;
     input = input.toLowerCase();
+
     output = `<div>➜testing input is ${input}</div>`;
+    if(!COMMANDS.hasOwnProperty(input)){
+        output += `<div class="output"> No such command ${input} </div>`;
+    }
+    else{
+        output += `<div class="output"> ${COMMANDS[input]} </div>`;
+    }
+
     terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
     user_input.value = "";
 }
@@ -23,7 +31,20 @@ const key = function keyEvent(e) {
     user_input.innerHTML = input + e.key;
 };
 
-
+const COMMANDS = {
+    help:
+      'Supported commands: ["about", "experience", "education", "skills", "contact"]',
+    about:
+      "Hello 👋, I'm Brian, not brain. I like chocolate and don't like carrots.",
+    skills:
+      'I can hold three eggs in my mouth at the same time. Can you?',
+    education:
+      "I learned to eat by myself at the age at 3. Impressive, isn't it?",
+    experience:
+      "I wrote \'hello world\' in python, and I felt I have the power in my hand!",
+    contact:
+      'You can never find me haha'
+  };
 
 
 
