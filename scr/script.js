75ec3f601708b11a93a3afbe12102f85bd09734c
Chen-Kuan Liao <czl5899@psu.edu>
2022-07-04 02:14:23 -0400

    remove ul and able to add elements

--- scr/script.js
+++ scr/script.js
@@ -1,16 +1,18 @@
 var commands = document.getElementById("commands");
 var user_input = document.getElementById("user_input");
+var terminal_outputs = document.getElementById("terminal_outputs");
 
-function execute(){
-    var newCommand = document.createElement("li");
-    newCommand.innerHTML = user_input.value;
-    commands.appendChild(newCommand);
+function execute(input){
+    let output;
+    input = input.toLowerCase();
+    output = `<div>➜testing input is ${input}</div>`;
+    terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
     user_input.value = "";
 }
 
 const key = function keyEvent(e) {
     user_input = document.getElementById("user_input");
-    const input = window.user_input.innerHTML;
+    const input = window.user_input.value;
 
     if (e.key === "Enter") {
         execute(input);
@@ -19,7 +21,7 @@ const key = function keyEvent(e) {
     }
 
     user_input.innerHTML = input + e.key;
-    };
+};
 
 
 
